c6100a7b755ed66325ae7d0ba5c8326f553da4ac
jan.vrbensky.021 <jan.vrbensky.021@pslib.cz>
2024-03-26 10:33:12 +0100

    first commit

---
+++ Joomla2.0/Models/Article.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Claims;
+
+namespace Joomla2._0.Models
+{
+    public class Article
+    {
+        public int ArticleId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime Createdat { get; set; } 
+        public Guid AuthorId {  get; set; }
+        [ForeignKey(nameof(AuthorId))]
+        public JUser Author { get; set; }
+    }
+}
---
+++ Joomla2.0/Models/JUserArticle.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Claims;
+
+namespace Joomla2._0.Models
+{
+    public class JUserArticle
+    {
+    }
+}
